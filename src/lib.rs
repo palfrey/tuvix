1acc9da534fb340d0c7a1b1af31087c329bd5f19
Tom Parker-Shemilt <palfrey@tevp.net>
2022-03-06 16:19:19 +0000

    Hacky, half-working ptrace+chroot work

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 use anyhow::{anyhow, bail, Context, Result};
 use libmount::BindMount;
+use nix::libc;
+use nix::sys::ptrace;
+use nix::sys::signal::Signal;
+use nix::sys::wait::{waitpid, WaitStatus};
+use nix::unistd::Pid;
 use reqwest::blocking::Client;
 use sha2::{Digest, Sha256};
+use spawn_ptrace::CommandPtraceSpawn;
 use starlark::collections::SmallMap;
 use starlark::environment::{Globals, GlobalsBuilder, Module};
 use starlark::eval::Evaluator;
@@ -11,10 +17,14 @@ use starlark::values::dict::Dict;
 use starlark::values::none::NoneType;
 use starlark::values::structs::StructBuilder;
 use starlark::values::{AllocValue, AnyLifetime};
+use std::collections::VecDeque;
+use std::ffi::CString;
 use std::fs::File;
+use std::io::Read;
 use std::os::unix::fs as unix_fs;
+use std::os::unix::prelude::CommandExt;
 use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::process::{Command, Stdio};
 use std::{env, fs};
 use tar::Archive;
 use xz2::read::XzDecoder;
@@ -22,6 +32,7 @@ use xz2::read::XzDecoder;
 #[derive(AnyLifetime)]
 struct Info {
     pub http_client: Client,
+    pub hash_dir: PathBuf,
 }
 
 fn check_hash_for_bytes(contents: &[u8], expected_hash: &str) -> Result<(), String> {
@@ -76,27 +87,73 @@ fn starlark_helpers(builder: &mut GlobalsBuilder) {
     }
 
     fn chdir(folder: &str) -> NoneType {
-        print!("CD to {folder}");
+        println!("CD to {folder}");
         env::set_current_dir(folder)?;
         Ok(NoneType)
     }
 
     fn run(command: &str) -> i32 {
-        let bits: Vec<_> = command.split(" ").collect();
-        let program = "/.bin/strace";
-        let output = Command::new(program)
-            .args(&bits)
-            .env_clear()
-            .output()
-            .context(format!("from command for {} {:?}", program, &bits))?;
-        if output.status.code() != Some(0) {
-            bail!(
-                "Failed to run {}: {}, {}",
-                command,
-                std::str::from_utf8(&output.stdout)?,
-                std::str::from_utf8(&output.stderr)?
-            );
+        let mut bits: VecDeque<_> = command.split(" ").collect();
+        let program = bits.pop_front().expect("pop program");
+        let hash_dir = eval
+            .extra
+            .unwrap()
+            .downcast_ref::<Info>()
+            .unwrap()
+            .hash_dir
+            .to_str()
+            .unwrap()
+            .as_bytes()
+            .to_owned();
+        println!("hash_dir: {}", std::str::from_utf8(&hash_dir).unwrap());
+        let mut child = unsafe {
+            Command::new(program)
+                .args(&bits)
+                .env_clear()
+                .pre_exec(move || {
+                    let store_dir = CString::new(&*hash_dir).unwrap();
+                    libc::chroot(store_dir.as_ptr());
+                    let root_dir = CString::new("/").unwrap();
+                    libc::chdir(root_dir.as_ptr());
+                    let zsh_dir = CString::new("zsh-5.8.1.tar/zsh-5.8.1").unwrap();
+                    libc::chdir(zsh_dir.as_ptr());
+                    Ok(())
+                })
         }
+        .current_dir("/")
+        .stdin(Stdio::null())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn_ptrace()
+        .expect("spawn");
+        let pid = Pid::from_raw(child.id() as i32);
+        ptrace::cont(pid, Signal::SIGCONT).expect("ptrace cont");
+        let code = loop {
+            match waitpid(pid, None).unwrap() {
+                WaitStatus::Exited(_, exit_code) => {
+                    println!("Child exited with {exit_code}");
+                    break exit_code;
+                }
+                WaitStatus::Stopped(_, _signal) => {
+                    // println!("Child stopped with {signal}");
+                    ptrace::cont(pid, Signal::SIGCONT).expect("ptrace cont");
+                    continue;
+                }
+                res => {
+                    println!("res: {:?}", res);
+                    continue;
+                }
+            };
+        };
+        if code != 0 {
+            drop(child.stdin.take());
+            let mut stdout = String::new();
+            child.stdout.unwrap().read_to_string(&mut stdout).unwrap();
+            let mut stderr = String::new();
+            child.stderr.unwrap().read_to_string(&mut stderr).unwrap();
+            bail!("Failed to run {}: '{}' '{}'", command, stdout, stderr);
+        }
+
         Ok(0)
     }
 }
@@ -140,6 +197,12 @@ fn setup_hashdir(hash_dir: &PathBuf) -> Result<()> {
         let dev_mount = BindMount::new("/dev", dev_path);
         dev_mount.mount().map_err(|err| anyhow!("{}", err))?;
     }
+    let proc_path = hash_dir.join("proc");
+    make_if_not_exists(&proc_path)?;
+    if !proc_path.join("version").exists() {
+        let proc_mount = BindMount::new("/proc", proc_path);
+        proc_mount.mount().map_err(|err| anyhow!("{}", err))?;
+    }
 
     Ok(())
 }
@@ -171,6 +234,7 @@ pub fn build_module(filename: &str) -> Result<()> {
     let mut eval: Evaluator = Evaluator::new(&module);
     let info = Info {
         http_client: Client::new(),
+        hash_dir: hash_dir.clone(),
     };
     eval.extra = Some(&info);
 
