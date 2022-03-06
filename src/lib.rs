a6099452df5a28c8e3cc41aacd84bfd07fe05d39
Tom Parker-Shemilt <palfrey@tevp.net>
2022-03-06 20:57:48 +0000

    Remove ptrace bits

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,7 @@
 use anyhow::{anyhow, bail, Context, Result};
 use libmount::BindMount;
-use nix::libc;
-use nix::sys::ptrace;
-use nix::sys::signal::Signal;
-use nix::sys::wait::{waitpid, WaitStatus};
-use nix::unistd::Pid;
 use reqwest::blocking::Client;
 use sha2::{Digest, Sha256};
-use spawn_ptrace::CommandPtraceSpawn;
 use starlark::collections::SmallMap;
 use starlark::environment::{Globals, GlobalsBuilder, Module};
 use starlark::eval::Evaluator;
@@ -18,13 +12,10 @@ use starlark::values::none::NoneType;
 use starlark::values::structs::StructBuilder;
 use starlark::values::{AllocValue, AnyLifetime};
 use std::collections::VecDeque;
-use std::ffi::CString;
 use std::fs::File;
-use std::io::Read;
 use std::os::unix::fs as unix_fs;
-use std::os::unix::prelude::CommandExt;
 use std::path::{Path, PathBuf};
-use std::process::{Command, Stdio};
+use std::process::Command;
 use std::{env, fs};
 use tar::Archive;
 use xz2::read::XzDecoder;
@@ -106,52 +97,18 @@ fn starlark_helpers(builder: &mut GlobalsBuilder) {
             .as_bytes()
             .to_owned();
         println!("hash_dir: {}", std::str::from_utf8(&hash_dir).unwrap());
-        let mut child = unsafe {
-            Command::new(program)
-                .args(&bits)
-                .env_clear()
-                .pre_exec(move || {
-                    let store_dir = CString::new(&*hash_dir).unwrap();
-                    libc::chroot(store_dir.as_ptr());
-                    let root_dir = CString::new("/").unwrap();
-                    libc::chdir(root_dir.as_ptr());
-                    let zsh_dir = CString::new("zsh-5.8.1.tar/zsh-5.8.1").unwrap();
-                    libc::chdir(zsh_dir.as_ptr());
-                    Ok(())
-                })
-        }
-        .current_dir("/")
-        .stdin(Stdio::null())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn_ptrace()
-        .expect("spawn");
-        let pid = Pid::from_raw(child.id() as i32);
-        ptrace::cont(pid, Signal::SIGCONT).expect("ptrace cont");
-        let code = loop {
-            match waitpid(pid, None).unwrap() {
-                WaitStatus::Exited(_, exit_code) => {
-                    println!("Child exited with {exit_code}");
-                    break exit_code;
-                }
-                WaitStatus::Stopped(_, _signal) => {
-                    // println!("Child stopped with {signal}");
-                    ptrace::cont(pid, Signal::SIGCONT).expect("ptrace cont");
-                    continue;
-                }
-                res => {
-                    println!("res: {:?}", res);
-                    continue;
-                }
-            };
-        };
-        if code != 0 {
-            drop(child.stdin.take());
-            let mut stdout = String::new();
-            child.stdout.unwrap().read_to_string(&mut stdout).unwrap();
-            let mut stderr = String::new();
-            child.stderr.unwrap().read_to_string(&mut stderr).unwrap();
-            bail!("Failed to run {}: '{}' '{}'", command, stdout, stderr);
+        let output = Command::new(program)
+            .args(&bits)
+            .env_clear()
+            .output()
+            .expect("spawn");
+        if output.status.code() != Some(0) {
+            bail!(
+                "Failed to run {}: '{}' '{}'",
+                command,
+                std::str::from_utf8(&output.stdout).unwrap(),
+                std::str::from_utf8(&output.stderr).unwrap()
+            );
         }
 
         Ok(0)
@@ -203,6 +160,9 @@ fn setup_hashdir(hash_dir: &PathBuf) -> Result<()> {
         let proc_mount = BindMount::new("/proc", proc_path);
         proc_mount.mount().map_err(|err| anyhow!("{}", err))?;
     }
+
+    let tmp_path = hash_dir.join("tmp");
+    make_if_not_exists(&tmp_path)?;
 
     Ok(())
 }
